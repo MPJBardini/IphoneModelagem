b0c695fb4b0e30e51b24b9ca2feed298a55daf3f
MPJBardini <jhony_bardini@hotmail.com>
2024-05-24 09:46:10 -0300

    ProjetoModelando Iphone

---
+++ src/navegador.java
@@ -0,0 +1,5 @@
+public interface navegador {
+    public void exibirPagina(String url);
+    public void adicionarNovaAba(String url);
+    public void atualizarPagina();
+}
---
+++ src/reprodutor.java
@@ -0,0 +1,5 @@
+public interface reprodutor {
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica();
+}
